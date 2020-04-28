a1eb08b03c010167c2ef041e149595c34a14667f
Nazim Uddin <umnazim@gmail.com>
2020-04-27 23:47:24 -0700

    map items to create components

--- my-notes/src/components/App.js
+++ my-notes/src/components/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+// import { Content } from 'react-mdl';
+
+function createNotes(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
