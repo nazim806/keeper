111efdb8a3a9209f0902f60a0323a701bdaafca7
Nazim Uddin <umnazim@gmail.com>
2020-04-27 23:22:38 -0700

    add notes file

--- my-notes/src/components/Footer.jsx
+++ my-notes/src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Footer() {
   const currentYear = new Date().getFullYear();
 
-  return <footer>
-      Copyright {currentYear}
-  </footer> 
+  return <footer>Copyright &copy; {currentYear}</footer>;
 }
 
 export default Footer;
