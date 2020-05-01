604a5e7ff583421975887c966bb9ad19aaf416f0
Nazim Uddin <umnazim@gmail.com>
2020-05-01 00:53:06 -0700

    create new note and pass it to an array

--- my-notes/src/components/App.js
+++ my-notes/src/components/App.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+
+import CreateArea from "./CreateArea";
 
 // import { Content } from 'react-mdl';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
   return (
     <div>
       <Header />
-
-      {notes.map((noteItem) => 
-        
-          <Note
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-          />
-        
-    )}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
 
       <Footer />
     </div>
