1f1bb3c0e9e9ada74c1ff8c7be7056e42170f8fa
Nazim Uddin <umnazim@gmail.com>
2020-05-01 21:47:16 -0700

    install prebuilt react components material-ui/icons material-ui/core

--- my-notes/src/components/CreateArea.jsx
+++ my-notes/src/components/CreateArea.jsx
@@ -28,7 +28,7 @@ function CreateArea(props) {
   }
   return (
     <div>
-      <form>
+      <form className = "create-note">
         <input
           name="title"
           onChange={handleChange}
